37b7efe6d8dcdce54c4f32d6caeecb803868405f
SuHyeon-Eleven <minsoohyeon1234@gmail.com>
2021-11-04 04:00:26 +0900

    로그인/회원가입 구현...아마..?

--- static/login.js
+++ static/login.js
@@ -1,7 +1,7 @@
 function login() {
 
-    let id_give = $("#user_id").val();
-    let pw_give = $("#password").val();
+    let id_give = $("#id_give").val();
+    let pw_give = $("#pw_give").val();
 
     if (id_give == "") {
         alert("아이디를 입력해주세요");
@@ -14,17 +14,18 @@ function login() {
 
     $.ajax({
         type: "POST",
-        url: "/login",
+        url: "/api/login",
         data: { id_give: id_give, pw_give: pw_give },
         success: function (response) {
             if (response['result'] == 'success') {
                 // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
                 // 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
                 $.cookie('mytoken', response['token']);
-                return window.location.href = '/'
+                window.location.href = '/'
             } else {
                 // 로그인이 실패시 에러메시지를 띄웁니다.
-                return alert(response['msg'])
+                
+                alert(response['msg'])
             }
         }
     })
