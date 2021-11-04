f3583a1ada4dbd36390e1a9b862a6bf20dbd9133
SuHyeon-Eleven <minsoohyeon1234@gmail.com>
2021-11-04 14:30:41 +0900

    empty commit

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ SECRET_KEY = 'SECRET'
 API_KEY= 'W7rRGCTEuCgKF9Ml%2FwKJbHCJf0duO218F3SYriSEGGFnjmztdsdfE9CmzyEcW8vma%2FwxwqteC1HIXU4bTgjjOg%3D%3D'
 API_URL=f'http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList?ServiceKey={API_KEY}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json'
 
-
 client = MongoClient('localhost', 27017)
 db = client.chabak
 
